8d579f53f2b381766804e6250f862d6645a43b5a
Ria Soam <ria.soam@olx.com>
2023-06-09 01:11:48 +0530

    route added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router";
 import MainPage from "./components/MainPage/MainPage";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
-  <MainPage />
+    <BrowserRouter>
+  <Routes>
+    <Route path={"/"} Component={MainPage}/>
+  </Routes>
+  </BrowserRouter>
 );
 }
