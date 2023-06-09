40e5ee07c0003175ca2d43eca4fa69574480e074
Ria Soam <ria.soam@olx.com>
2023-06-09 11:52:02 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { Route, Routes } from "react-router";
 import MainPage from "./components/MainPage/MainPage";
-import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
-    <BrowserRouter>
-  <Routes>
-    <Route path={"/ria-2511/Scratch-project"} Component={MainPage}/>
-  </Routes>
-  </BrowserRouter>
+  //   <BrowserRouter>
+  // <Routes>
+  //   <Route path={"/ria-2511/Scratch-project"} Component={MainPage}/>
+  // </Routes>
+  // </BrowserRouter>
+  <MainPage/>
 );
 }
